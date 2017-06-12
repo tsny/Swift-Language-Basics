d840dbee9f1e6c73bfcfd9f5ff4724baaf304f54
Taylor Snyder <tsny@taylors-mbp.t-mobile.com>
2017-06-11 19:23:18 -0500

    init commit

---
+++ Swift Language Basics/Swift Language Basics/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swift Language Basics
+//
+//  Created by Taylor Snyder on 6/11/17.
+//  Copyright © 2017 Taylor Snyder. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
